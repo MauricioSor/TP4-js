b2bbcfb7e23c05360e8eee7178bb0460b8c3ed21
Mauricio <mauricioutn2017@gmail.com>
2023-03-28 15:46:14 -0300

    Correccion a la estructura del proyecto

--- js/1.js
+++ js/1.js
@@ -1,17 +1,10 @@
 const auto ={
-    color:"rojo",marca:"Chevrolet",modelo:2020,
+    color:"rojo",marca:"Chevrolet",
+    modelo:2020,
     encender:() =>{
-    encender=alert("Desea encender su auto?");
     console.log("El auto esta encendido");
-    }
-    ,apagar:()=>{
-        apagar=alert("Desea apagar su auto?");
+    },
+    apagar:()=>{
         console.log("Auto apagado");
     }
     };
-    while(confirm("Desea bajarse del auto?")!=true){
-        auto.encender();
-        if(confirm!="Desea apagar el auto?"==true){
-            auto.apagar();
-        }
-    }
