54fc98994bc5aa80a1bf4d1689d2ebde3e5c7870
Mauricio <mauricioutn2017@gmail.com>
2023-04-07 00:16:43 -0300

    Listo el ejercicio 9

--- js/9.js
+++ js/9.js
@@ -1,4 +1,4 @@
-class animal{
+class Animal{
     #nombre 
     #edad
     constructor(nombre,edad){
@@ -22,22 +22,75 @@ class animal{
     return this.#edad;
     }
     emitirSonido(){
-    alert("Un animal esta emitiendo sonido");
+    alert(`Un animal llamado ${this.#nombre} esta emitiendo sonido`);
     }
 }
-class Perro extends animal(){
+class Perro extends Animal{
     #nombre;
     #edad;
-    constructor(nombre,edad){
-    super(nombre,edad)
+    #raza;
+    constructor(nombre,edad,raza){
+    super(nombre,edad);
+    this.#raza=raza;
+    }
+    set raza(nuevaRaza){
+    if(nuevaRaza!=undefined){
+        return this.#raza=nuevaRaza;
+    }
+    }
+    get raza(){
+    return this.#raza;
     }
     emitirSonido(){
-        alert("El perro esta emitiendo sonido");
+        alert(`El perro de nombre ${this.nombre} de raza ${this.raza} esta emitiendo sonido`);
         }
 }
-class Gato extends animal(){
+class Gato extends Animal{
+    #nombre;
+    #edad;
+    #raza;
+    constructor(nombre,edad,raza){
+    super(nombre,edad);
+    this.#raza=raza;
+    }
+    set raza(nuevaRaza){
+    if(nuevaRaza!=undefined){
+    return this.#raza=nuevaRaza;
+    }
+    }
+    get raza(){
+    return this.#raza;
+    }
     emitirSonido(){
-    alert("El gato esta emitiendo sonido");
+    alert(`El gato ${this.nombre} de raza ${this.raza} esta emitiendo sonido`);
     }
 }
-const tom = new Gato();
+function tipoAnimal(x){
+    let  Animal;
+    switch(x){
+        case "Spyke":
+         Animal = spyke;
+         break;
+        case "Jerry":
+         Animal = jerry;
+         break;
+        case "Tom":
+         Animal = tom;
+         break;
+    }
+    return Animal;
+}
+function menu(Animal){
+    alert("Ingrese el numero de lo que desee hacer:");
+    let x = parseInt(prompt("1- Saludar "));
+    switch (x){
+    case 1:
+        Animal.emitirSonido();
+        break;
+    }
+    }
+const jerry = new Animal("Jerry",3);
+const tom = new Gato("Tom",6,"mestizo")
+const spyke = new Perro("Spyke",2,"Dogo");
+let x= prompt("Que tipo de animal es? Spyke , Tom o Jerry?");
+menu(tipoAnimal(x));
